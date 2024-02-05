62035191b9f0ff4e8dfe449aab0183dc8d23965e
ParticleG <particle_g@outlook.com>
2024-02-05 10:21:01 +0800

    - Update MemoryPayloads design

--- models/MemoryPayloads.cc
+++ models/MemoryPayloads.cc
@@ -1,22 +1,48 @@
+#include <regex>
+
 #include <models/MemoryPayloads.h>
 
-using namespace std;
 using namespace models;
+using namespace std;
 
-SimpleString::SimpleString(const std::string_view str) {
+namespace {
+    regex recordRegex(
+        R"~(Symbol="(.*?)";Type="(.*?)";Project="(.*?)";File="(.*?)";)~"s +
+        R"~(lnFirst="(.*?)";lnLim="(.*?)";lnName="(.*?)";ichName="(.*?)";Instance="(.*?)")~"s
+    );
+}
+
+SimpleString::SimpleString(std::string_view str) {
     _data.lengthLow = static_cast<uint8_t>(str.length() & 0xFF);
-    _data.lengthHigh = static_cast<uint8_t>(str.length() >> 8 & 0xFF);
-    memcpy(_data.content, str.data(), str.length());
+    _data.lengthHigh = static_cast<uint8_t>((str.length() >> 8) & 0xFF);
+    ranges::copy(str, _data.content);
 }
 
 string SimpleString::str() const {
     return {_data.content, _data.lengthLow + static_cast<uint32_t>(_data.lengthHigh << 8)};
 }
 
-SimpleString::Data* SimpleString::data() {
-    return &_data;
+string SymbolName::name() const {
+    return {_data.name};
 }
 
-SymbolLocation::Data* SymbolLocation::data() {
-    return &_data;
+optional<SymbolRecord::Record> SymbolRecord::parse() const {
+    const auto content = this->str();
+    if (smatch match;
+        regex_search(content, match, recordRegex) && match.size() == 10) {
+        return Record{
+            match[4],
+            match[3],
+            match[1],
+            match[2],
+            {
+                static_cast<uint32_t>(stoul(match[8])),
+                static_cast<uint32_t>(stoul(match[7]))
+            },
+            static_cast<uint32_t>(stoul(match[9])),
+            static_cast<uint32_t>(stoul(match[6])),
+            static_cast<uint32_t>(stoul(match[5])),
+        };
+    }
+    return nullopt;
 }
--- models/MemoryPayloads.h
+++ models/MemoryPayloads.h
@@ -1,39 +1,90 @@
 #pragma once
 
 #include <cstdint>
+#include <optional>
 #include <string>
 
-namespace models {
-    class SimpleString {
-        struct Data {
-            uint8_t lengthLow, lengthHigh;
-            char content[4094];
-        };
+#include <types/CaretPosition.h>
 
+namespace models {
+    template<typename Data>
+    class PayloadBase {
+    public:
+        virtual ~PayloadBase() = default;
+
+        [[nodiscard]] const void* data() const {
+            return &_data;
+        }
+
+        [[nodiscard]] void* data() {
+            return &_data;
+        }
+
+        [[nodiscard]] uint32_t size() const {
+            return sizeof(_data);
+        }
+
+    protected:
+        Data _data;
+    };
+
+    struct SimpleStringData {
+        uint8_t lengthLow, lengthHigh;
+        char content[4094];
+    };
+
+    struct SymbolBufferData {
+        uint32_t data1, data2;
+        uint32_t reserved1[2];
+        uint32_t data3, data4, data5, data6;
+        uint32_t reserved2[4];
+        int32_t reserved3[8];
+        uint32_t data7, data8;
+        uint32_t reserved4[8];
+        int32_t reserved5;
+        uint32_t reserved6[10];
+        int32_t reserved7;
+        uint32_t reserved8[22];
+    };
+
+    struct SymbolListData {
+        uint32_t size;
+        uint8_t data[4092];
+    };
+
+    struct SymbolNameData {
+        uint32_t nestingDepth; ///< The nesting depth of the symbol. Starts at 1.
+        char name[4092];
+    };
+
+    class SimpleString : public PayloadBase<SimpleStringData> {
     public:
         SimpleString() = default;
 
         explicit SimpleString(std::string_view str);
 
         [[nodiscard]] std::string str() const;
-
-        Data* data();
-
-    private:
-        Data _data{};
     };
 
-    class SymbolLocation {
-        struct Data {
-            uint8_t lineStart, lineEnd, characterStart, characterEnd;
-            char content[4092];
-        };
+    using SymbolBuffer = PayloadBase<SymbolBufferData>;
+
+    using SymbolList = PayloadBase<SymbolListData>;
+
+    using SymbolListHandle = SymbolList *;
+
+    class SymbolName final : public PayloadBase<SymbolNameData> {
     public:
-        SymbolLocation() = default;
+        [[nodiscard]] std::string name() const;
+    };
 
-        Data* data();
+    class SymbolRecord final : public SimpleString {
+    public:
+        struct Record {
+            std::string file, project, symbol, type;
+            types::CaretPosition namePosition;
+            uint32_t instanceIndex, lineEnd, lineStart;
+        };
 
-    private:
-        Data _data{};
+        [[nodiscard]] std::optional<Record> parse() const;
     };
 }
