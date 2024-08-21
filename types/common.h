4727953c8dc8dac2b5c364126fc2200c07f43768
ParticleG <particle_g@outlook.com>
2024-08-21 16:38:20 +0800

    - Add `Overloaded` for std::visit

--- types/common.h
+++ types/common.h
@@ -5,6 +5,11 @@
 namespace types {
     constexpr auto UM_KEYCODE = 0x07E9;
 
+    template<class... Ts>
+    struct Overloaded : Ts... {
+        using Ts::operator()...;
+    };
+
     using Keycode = unsigned int;
     using Time = std::chrono::time_point<std::chrono::high_resolution_clock>;
 }
