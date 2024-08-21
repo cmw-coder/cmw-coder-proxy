04bc20f2cd1a979d5795ac29c4e15ff159aaf49b
ParticleG <particle_g@outlook.com>
2024-08-21 16:38:35 +0800

    - Remove unused methods

--- types/CaretPosition.cc
+++ types/CaretPosition.cc
@@ -5,17 +5,6 @@ using namespace types;
 CaretPosition::CaretPosition(const uint32_t character, const uint32_t line)
     : character(character), line(line), maxCharacter(line) {}
 
-CaretPosition& CaretPosition::addCharactor(const int64_t charactor) {
-    this->character += charactor;
-    this->maxCharacter += charactor;
-    return *this;
-}
-
-CaretPosition& CaretPosition::addLine(const int64_t line) {
-    this->line += line;
-    return *this;
-}
-
 void CaretPosition::reset() {
     this->character = 0;
     this->line = 0;
@@ -44,14 +33,6 @@ bool CaretPosition::operator>(const CaretPosition& other) const {
 
 bool CaretPosition::operator>=(const CaretPosition& other) const {
     return this->line > other.line || (this->line == other.line && this->character >= other.character);
-}
-
-CaretPosition& CaretPosition::operator+=(const CaretPosition& other) {
-    return addCharactor(other.character).addLine(other.line);
-}
-
-CaretPosition& CaretPosition::operator-=(const CaretPosition& other) {
-    return addCharactor(-other.character).addLine(-other.line);
 }
 
 CaretPosition types::operator+(const CaretPosition& lhs, const CaretPosition& rhs) {
--- types/CaretPosition.h
+++ types/CaretPosition.h
@@ -11,10 +11,6 @@ namespace types {
 
         CaretPosition(uint32_t character, uint32_t line);
 
-        CaretPosition& addCharactor(int64_t charactor);
-
-        CaretPosition& addLine(int64_t line);
-
         void reset();
 
         bool operator<(const CaretPosition& other) const;
@@ -29,39 +25,8 @@ namespace types {
 
         bool operator>=(const CaretPosition& other) const;
 
-        CaretPosition& operator+=(const CaretPosition& other);
-
-        CaretPosition& operator-=(const CaretPosition& other);
-
         friend CaretPosition operator+(const CaretPosition& lhs, const CaretPosition& rhs);
 
         friend CaretPosition operator-(const CaretPosition& lhs, const CaretPosition& rhs);
     };
 }
-
-// #include <format>
-// #include <string>
-//
-// template<>
-// struct std::formatter<types::CaretPosition> : std::formatter<std::string> {
-//     bool detailed{false};
-//
-//     constexpr auto parse(std::format_parse_context& context) {
-//         auto it = context.begin();
-//         const auto end = context.end();
-//         if (it != end && (*it == 'm')) detailed = (*it++) == 'm';
-//         if (it != end && *it != '}') throw format_error("Invalid format");
-//         return it;
-//     }
-//
-//     template<class FormatContext>
-//     auto format(const types::CaretPosition& CaretPosition, FormatContext& context) {
-//         std::string base_str = "line: " + to_string(CaretPosition.line) + " character: " + to_string(
-//                                    CaretPosition.character);
-//         if (detailed) {
-//             std::string op_str = base_str + " maxCharacter: " + to_string(CaretPosition.maxCharacter);
-//             return formatter<std::string>::format(op_str, context);
-//         }
-//         return formatter<std::string>::format(base_str, context);
-//     }
-// };
